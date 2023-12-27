68cd58b63cb9b6f8034288f80e548e5a46d18bbb
heyaadityaprakash <prakashaaditya68@gmail.com>
2023-12-27 20:46:51 +0530

    created a lower tirangle of * pattern

---
+++ pattern.java
@@ -0,0 +1,14 @@
+public class pattern {
+    public static void main(String[] args) {
+        //print a lower right triangle of stars.
+        for(int i=0;i<=4;i++){
+            for(int j=i;j<=4;j++){
+                 System.out.print("*");
+
+            }
+            System.out.println(" ");
+           
+        }
+    }
+    
+}
